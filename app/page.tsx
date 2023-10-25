1b31ccd69ee7443711b901bdf849cfbda037533a
Luciano Rodríguez <luciano.rodriguez59@gmail.com>
2023-10-25 20:55:40 -0300

    modified title

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>DISCORD CLONE</h1>
+      <h1>DISCORD</h1>
     </main>
   )
 }
