48c9524038532ebc71838caba7a06cd40a46135a
Giovanni <giovannibarolin@gmail.com>
2023-11-01 21:30:53 -0300

    Creating the layout file on messages component

---
+++ app/channels/messages/layout.tsx
@@ -0,0 +1,22 @@
+import UserChat from '@/app/components/UserChat'
+import Image from 'next/image'
+
+export default function DirectMessages({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+        <section className="bg-slate-800 pl-4">
+          <h2>
+            Mensajes Directos <span>+</span>
+          </h2>
+          <hr></hr>
+          <ul className="mt-2">
+            <UserChat isActive={true} />
+            <UserChat isActive={false} />
+          </ul>
+        </section>
+        <main>
+            {children}
+        </main>
+    </>
+  )
+}
--- app/channels/messages/page.tsx
+++ app/channels/messages/page.tsx
@@ -3,15 +3,8 @@ import Image from 'next/image'
 
 export default function Channel() {
   return (
-    <section className="bg-slate-800 pl-4">
       <h2>
         Mensajes Directos <span>+</span>
       </h2>
-
-      <ul className="mt-2">
-        <UserChat isActive={true} />
-        <UserChat isActive={false} />
-      </ul>
-    </section>
   )
 }
