2bef464d82515a14c700b05ca01a8af3fdb5f223
Giovanni <giovannibarolin@gmail.com>
2023-11-09 19:27:05 -0300

    dividing the main screen

--- app/(sharedLayout)/friends/screens/online/onlineScreen.tsx
+++ app/(sharedLayout)/friends/screens/online/onlineScreen.tsx
@@ -4,9 +4,21 @@ import { BiSearch } from 'react-icons/bi'
 export default function OnlineScreen(){
 
     return(
-        <div className="w-[90%] h-[90%] flex justify-start items-start pt-4">
+        <div className="w-[100%] h-[100%] flex justify-start items-start">
+            <section className="flex flex-col justify-start items-start w-[70%] h-[100%] px-5 border-r-2 border-solid border-[#3f4147] pt-4">
             <Input className={["w-[100%]","h-[35px]","bg-[#1e1f22]","active: border-none","rounded-md","px-1"]} icon={<BiSearch />}/>
-        <h1>En línea!</h1>
+            <h6 className="text-[#949ba4] pt-8 text-xs">TODOS LOS AMIGOS - 3</h6>
+            </section>
+            <section className=" h-[100%] px-5 pt-4 w-[30%]">
+                <h3 className="text-[#f2f3f5] font-bold self-start mb-4">Activo ahora</h3>
+                <div className="flex flex-col justify-center items-center p-4">
+                    <p className="text-[#f2f3f5] text-xs mb-1">Por ahora está todo tranquilo...</p>
+                    <p className="text-[#949ba4] text-xs text-center">
+                        Cuando un amigo empiece a realizar una actividad como jugar o hablar por voz, ¡te lo mostraremos aqui!
+                    </p>
+                </div>
+            </section>
+
       </div>
     )
 }
