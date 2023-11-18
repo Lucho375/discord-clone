b2d3c64b3228b898b1b193170eaa49cea1c5038c
Giovanni <giovannibarolin@gmail.com>
2023-11-17 21:44:15 -0300

    updating the MessagesBottomBar component

--- app/ui/MessagesBottomBar.tsx
+++ app/ui/MessagesBottomBar.tsx
@@ -1,21 +1,31 @@
+'use client'
 import Input from '@/app/ui/Input'
 import { FaCirclePlus } from 'react-icons/fa6'
 import { IoIosGift } from 'react-icons/io'
 import { MdGifBox } from 'react-icons/md'
+import NavButton from './navigationbutton'
+import { useState } from 'react'
 
 interface IBottomProps {
   userName: string
 }
 export default function BottomBar({ userName }: IBottomProps) {
+  const [hover, setHover] = useState(false)
   return (
-    <div className="w-full h-10 flex justify-center items-center rounded-md bg-[#383a40] px-4 gap-3 sticky self-end">
-      <FaCirclePlus size="1.5rem" color="#b5bac1" />
+    <div className="w-full h-10 flex justify-center items-center rounded-md bg-[#383a40] px-4 sticky self-end">
+      <NavButton id="add" icon={<FaCirclePlus size="1.5rem" color={`${hover ? '#f2f4f7' : '#b5bac1'}`} />} hover />
       <Input
         className={['w-full', 'bg-background-100', 'h-full', 'text-[#b5bac1]', 'text-sm']}
         placeHolder={`Enviar mensaje a ${userName}`}
       />
-      <IoIosGift size="1.5rem" color="#b5bac1" />
-      <MdGifBox size="1.5rem" color="#b5bac1" />
+      <NavButton
+        id="gift"
+        icon={<IoIosGift size="1.5rem" color={`${hover ? '#f2f4f7' : '#b5bac1'}`} />}
+        hover
+        tooltipPosition="top"
+        tooltipMessage="¡Mejora a tus amigos! Regalales increibles ventajas de chat con Nitro."
+      />
+      <NavButton id="gifcard" icon={<MdGifBox size="1.5rem" color={`${hover ? '#f2f4f7' : '#b5bac1'}`} />} hover />
     </div>
   )
 }
